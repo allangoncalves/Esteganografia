8de2be5c843aaa24becc3b30dcff7a49e31e2ef5
Allan Gonçalves <allangoncalves@outlook.com>
2017-08-17 15:36:28 -0300

    Add files via upload

--- stegano.py
+++ stegano.py
@@ -3,11 +3,14 @@ import sys
 
 if sys.argv[2] == 'encode':
 	image = Image.open(sys.argv[1])
-	length = image.size
-	binarized = imageBin(image)
-	modified = encode(textBin(sys.argv[3]),binarized)
-	rgb = imageRGB(modified)
-	imageSave(rgb, sys.argv[1], length)
+	if len(sys.argv[1])*8 < len(image.getdata()*3):
+		length = image.size
+		binarized = imageBin(image)
+		modified = encode(textBin(sys.argv[3]),binarized)
+		rgb = imageRGB(modified)
+		imageSave(rgb, sys.argv[1], length)
+	else:
+		print("message doesn't fit.")
 elif sys.argv[2] == 'decode':
 	image = Image.open('modified_'+sys.argv[1])
 	binarized = imageBin(image)
